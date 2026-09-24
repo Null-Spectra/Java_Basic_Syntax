b1c86d2e3af0d9b178294ebd4e2d38f56f504f51
Shaik Rezwan Ahmmed Rafi <207450248+Null-Spectra@users.noreply.github.com>
2026-09-25 01:59:05 +0600

    video 7

---
+++ seven/main/Main.java
@@ -0,0 +1,17 @@
+package seven.main;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = -1;
+        switch(x) {
+            case 1 + 3 - 5:
+                System.out.println("Bangladesh");
+                break;
+            case 2:
+                System.out.println("USA");
+                break;
+            default:
+                System.out.println("Out of earth");
+        }
+    }
+}
