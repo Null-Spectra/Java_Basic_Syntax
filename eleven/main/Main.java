500208b61cede1190fb0cbf9daa8a5c541d9320e
Shaik Rezwan Ahmmed Rafi <207450248+Null-Spectra@users.noreply.github.com>
2026-09-25 02:11:17 +0600

    video 11

---
+++ eleven/main/Main.java
@@ -0,0 +1,11 @@
+package eleven.main;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = -30;
+        do {
+            System.out.println("Hi");
+            x = x++;
+        }while(x >= -25);
+    }
+}
