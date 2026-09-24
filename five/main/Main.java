668df42822ae766eccd0e6d0a3837d51b5d12168
Shaik Rezwan Ahmmed Rafi <207450248+Null-Spectra@users.noreply.github.com>
2026-09-25 01:38:28 +0600

    video 5

---
+++ five/main/Main.java
@@ -0,0 +1,13 @@
+package five.main;
+
+public class Main {
+    public static void main(String[] args) {
+        char ch = 'r';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("Vowel");
+        }else {
+            System.out.println("Consonant");
+        }
+    }
+}
