126e85f604b3c84552b92ea5317f959d5a5d4329
Shaik Rezwan Ahmmed Rafi <207450248+Null-Spectra@users.noreply.github.com>
2026-09-25 01:19:22 +0600

    video 2

---
+++ two/main/Main.java
@@ -0,0 +1,10 @@
+package two.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Bangladesh\n");
+        System.out.print("Dhaka\n");
+        System.out.print("Saidpur\n");
+        System.out.print("Bangladesh\tDhaka\tSaidpur\n");
+    }
+}
